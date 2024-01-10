4925d81732220588dea516d64008c9aab0ecc1bc
nick <nick.buoncristiani@eluv.io>
2024-01-10 13:24:56 -0800

    fixed package name

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "elv_client_py",
-    packages=find_packages(),
+    name = "elv-client-py",
+    version='0.1',
+    packages=['elv_client_py'],
     install_requires=['requests==2.31.0', 'base58==2.1.1'],
 )
