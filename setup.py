1619e1e656e792c04eadfb49c7f0a3d8126b4361
nick <nick.buoncristiani@eluv.io>
2024-01-10 13:09:16 -0800

    removed hyphens in package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "elv-client-py",
+    name = "elv_client_py",
     packages=find_packages(),
     install_requires=['requests==2.31.0', 'base58==2.1.1'],
 )
